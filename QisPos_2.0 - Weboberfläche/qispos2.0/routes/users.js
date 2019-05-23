cd81b8e9010db4971b845013df36700ca5e0722d
JuleMaass <39371614+JuleMaass@users.noreply.github.com>
2019-05-23 17:04:14 +0200

    Funktionierende Login

--- QisPos_2.0 - Weboberfläche/qispos2.0/routes/users.js
+++ QisPos_2.0 - Weboberfläche/qispos2.0/routes/users.js
@@ -1,97 +1,103 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcryptjs');
-const passport = require('passport');
 var mysql = require('mysql');
 var config = require('../config');
 
-var test;
 
 const db = mysql.createConnection(config.db);
 
-
+db.connect((err) => {
+  if (err) {
+    console.log('Error connecting to Db');
+    return;
+  }
+  console.log('Connection established');
+});
 
 
 
 //Login Page/view
-router.get('/login', (req, res) => res.render('login'));
+router.get('/login', (req, res) => res.render('login', {
+  error: ''
+}));
 
 //Register Page/view
 router.get('/register', (req, res) => res.render('register'));
 
 //Register handle
 router.post('/register', (req, res) => {
-  const {benutzername, id, email, PW } = req.body;
+  const {
+    benutzername,
+    id,
+    email,
+    PW
+  } = req.body;
   let errors = [];
 
   //check requires fields
-  if(!benutzername || !id || !email || !PW ){
-    errors.push({msg: 'Please fill in all fields'});
+  if (!benutzername || !id || !email || !PW) {
+    errors.push({
+      msg: 'Please fill in all fields'
+    });
 
   }
-if(errors.legth > 0){
-res.render('register', {
-  errors,
-  benutzername,
-  id,
-  email,
-  PW
-})
-}else{
-  res.send('pass')
-}
- 
+  if (errors.length > 0) {
+    res.render('register', {
+      errors,
+      benutzername,
+      id,
+      email,
+      PW
+    })
+  } else {
+    res.send('pass')
+  }
+
 });
 
 
 //Login handle
 router.post('/login', (req, res) => {
-
-    const {email, password } = req.body;
+  const {
+    email,
+    password
+  } = req.body;
   let errors = [];
 
-  // //check requires fields
-  // if(!email || !password ){
-  //   errors.push({msg: 'Please fill in all fields'});
+  //check requires fields
+  if (!email || !password) {
 
-    db.connect(function(err) {
-      if (err) throw err;
-      var sqlabfrage = "SELECT benutzername FROM student WHERE email = ? AND PW = ?;";
-      db.query(sqlabfrage,[email,password],(err, result)=> {
-        if (err) {
-          res.render('login', {
-              errors,
-              email,
-              password
-            });
-        };
-
-        test = result[0].benutzername;
-        res.render('dashboard', {
-          user: test
-        })
-      });
+    errors.push({
+      msg: 'Please fill in all fields'
     });
+  }
+
+  var sqlabfrage = "SELECT benutzername FROM student WHERE email = ? AND PW = ?;";
+  db.query(sqlabfrage, [email, password], (err, result) => {
+    if (result[0] == null) {
+      errors.push({
+        msg: 'Bitte geben Sie die richtigen Nutzerdaten ein.'
+      });
+    }
+
+    if (errors.length > 0) {
+
+      // console.log(errors.length)
+      console.log(errors[0].msg)
+      res.render('login', {
+        error: errors[0].msg
+      })
+    } else {
+     var nutzername = result[0].benutzername;
+      res.render('dashboard', {
+        user: nutzername
+      })
+    }
+  });
 
   
-// if(errors.legth > 0){
-// res.render('login', {
-//   errors,
-//   email,
-//   password
-// })
-// }else{
 
-// }
 });
 
-
-
-// Dashboard
-router.get('/dashboard', (req, res) =>
-  res.render('dashboard', {
-    user: req.user
-  })
-);
 
 module.exports = router;
